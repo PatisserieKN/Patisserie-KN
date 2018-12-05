54acfda15c3fa557938d85cf1e145983cb745725
StarrK <tony.kino.3@gmail.com>
2018-12-05 16:30:00 +0100

    Add validations to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  validates :name, presence: true,
+                   length: { minimum: 3, maximum: 63 }
+
+  validates :description, presence: true,
+                          length: { minimum: 3, maximum: 1023 }
+
+  validates :price, presence: true
+
+  validates :category, presence: true,
+                       length: { minimum: 3, maximum: 63 }
 end
