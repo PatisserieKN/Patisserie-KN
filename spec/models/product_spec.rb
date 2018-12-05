fd2fb5fe2770b89af082c387552517b588e4a852
StarrK <tony.kino.3@gmail.com>
2018-12-05 21:58:56 +0100

    Refactor name tests with let keyword

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
 
-  before { @product = build(:product) }
+  let(:product) { build(:product) }
 
   it 'should be valid' do
-    expect(@product.valid?).to be true
+    expect(product.valid?).to be true
   end
 
   context 'name' do
     it 'should be present' do
-      @product.name = ' ' * 6
-      expect(@product.valid?).to be false
+      product.name = ' ' * 6
+      expect(product.valid?).to be false
     end
 
     it 'should not be too long' do
-      @product.name = 'a' * 64
-      expect(@product.valid?).to be false
+      product.name = 'a' * 64
+      expect(product.valid?).to be false
     end
 
     it 'should not be too short' do
-      @product.name = 'a' * 2
-      expect(@product.valid?).to be false
+      product.name = 'a' * 2
+      expect(product.valid?).to be false
     end
   end
 end
