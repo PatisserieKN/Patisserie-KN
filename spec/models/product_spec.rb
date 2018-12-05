9b8cd20a28aed57e81164d2a3f76d5acd9b01605
StarrK <tony.kino.3@gmail.com>
2018-12-05 21:44:45 +0100

    Add firsts tests to validate name attribute

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should be valid' do
+    product = build(:product)
+    expect(product.valid?).to be true
+  end
+
+  context 'name' do
+    it 'should be present' do
+      product = build(:product)
+      product.name = ' ' * 6
+      expect(product.valid?).to be false
+    end
+
+    it 'should not be too long' do
+      product = build(:product)
+      product.name = 'a' * 64
+      expect(product.valid?).to be false
+    end
+
+    it 'should not be too short' do
+      product = build(:product)
+      product.name = 'a' * 2
+      expect(product.valid?).to be false
+    end
+  end
 end
