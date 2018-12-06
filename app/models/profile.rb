9989a86dd71971ccdaef7ecd0cc4c04467099058
Félix Perez <felix2@hotmail.fr>
2018-12-06 18:26:34 +0100

    Create the Profile model, adding validates and tests

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,7 @@
 class Profile < ApplicationRecord
+	validates :first_name, presence: true,
+						   lenght: { minimum: 2, maximum: 100}
+	validates :last_name, presence: true,
+						   lenght: { minimum: 2, maximum: 100}
+	validates :phone_number, presence: true
 end
--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :profile do
-    
+    first_name { "MyFName" }
+    last_name { "MyLName" }
+    phone_number { "0123456789" }
   end
 end
