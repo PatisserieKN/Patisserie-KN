2c91116bbe14d326f2bc9c6db3231204f856313e
Félix Perez <felix2@hotmail.fr>
2018-12-06 18:44:33 +0100

    Adding associations, might change

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,6 @@ class Profile < ApplicationRecord
 	validates :last_name, presence: true,
 						   lenght: { minimum: 2, maximum: 100}
 	validates :phone_number, presence: true
+
+	belongs_to :user
 end
