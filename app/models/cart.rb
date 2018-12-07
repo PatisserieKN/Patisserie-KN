49e191488ccd92a73a64ca0b12768a50793ded57
StarrK <tony.kino.3@gmail.com>
2018-12-07 14:53:18 +0100

    Add association

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ApplicationRecord
+  belongs_to :cart
   validates :paid, inclusion: { in: [true, false] }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :products
   validates :name, presence: true,
                    length: { minimum: 3, maximum: 63 }
 
