a1c63a9c187a06c77b6258b250d311e7000f9ed2
StarrK <tony.kino.3@gmail.com>
2018-12-07 12:02:36 +0100

    Add shoulda matchers tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,19 @@
 class Product < ApplicationRecord
-  validates :name, presence: true,
-                   length: { minimum: 3, maximum: 63 }
+  # validates :name, presence: true,
+  #                  length: { minimum: 3, maximum: 63 }
+  #
+  # validates :description, presence: true,
+  #                         length: { minimum: 3, maximum: 1023 }
+  #
+  # validates :price, presence: true
+  #
+  # validates :category, presence: true,
+  #                      length: { minimum: 3, maximum: 63 }
+  
 
-  validates :description, presence: true,
-                          length: { minimum: 3, maximum: 1023 }
+  validates_presence_of :name, :description, :price, :category
+  validates_length_of :name, minimum: 3, maximum: 63
+  validates_length_of :description, minimum: 3, maximum: 1023
+  validates_length_of :category, minimum: 3, maximum: 63
 
-  validates :price, presence: true
-
-  validates :category, presence: true,
-                       length: { minimum: 3, maximum: 63 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -65,4 +65,24 @@ RSpec.describe Product, type: :model do
       expect(product.valid?).to be false
     end
   end
+
+  # Shoulda matchers tests
+  context 'validation tests' do
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:description)}
+    it { is_expected.to validate_presence_of(:price)}
+    it { is_expected.to validate_presence_of(:category)}
+
+    it { is_expected.to validate_length_of(:name).
+                        is_at_least(3).is_at_most(63)}
+
+    it { is_expected.to validate_length_of(:description).
+                        is_at_least(3).is_at_most(1023)}
+
+    it { is_expected.to validate_length_of(:category).
+                        is_at_least(3).is_at_most(63)}
+  end
+
+
+
 end
