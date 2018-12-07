8f48e0e52e9061b263299d2f7d2956fb0de496d9
StarrK <tony.kino.3@gmail.com>
2018-12-07 11:20:24 +0100

    Add a nested random factory to product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,5 +4,12 @@ FactoryBot.define do
     description { "A beautiful cake" }
     price { "9.99" }
     category { "Pastry" }
+
+    factory :ramdom_product do
+      name { Faker::Food.fruits }
+      description { Faker::Food.description }
+      price { Faker::Number.decimal(2, 2) }
+      category { Faker::Food.dish }
+    end
   end
 end
