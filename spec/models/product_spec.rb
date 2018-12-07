15bbd6cc2dcf4e020b596e735e7ad0e7bf811342
StarrK <tony.kino.3@gmail.com>
2018-12-07 19:29:52 +0100

    Minor refactoring

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it { is_expected.to belong_to(:cart)}
-  it { is_expected.to validate_presence_of(:name)}
-  it { is_expected.to validate_presence_of(:description)}
-  it { is_expected.to validate_presence_of(:price)}
-  it { is_expected.to validate_presence_of(:category)}
+  it { is_expected.to belong_to(:cart) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_presence_of(:category) }
 
   it { is_expected.to validate_length_of(:name).
-                      is_at_least(3).is_at_most(63)}
+                      is_at_least(3).is_at_most(63) }
 
   it { is_expected.to validate_length_of(:description).
-                      is_at_least(3).is_at_most(1023)}
+                      is_at_least(3).is_at_most(1023) }
 
   it { is_expected.to validate_length_of(:category).
-                      is_at_least(3).is_at_most(63)}
+                      is_at_least(3).is_at_most(63) }
 
   let(:product) { create(:product) }
   it 'is creatable' do
     expect{ product }.to change(Product, :count).by(1)
   end
-
 end
