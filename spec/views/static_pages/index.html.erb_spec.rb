82258f9aed25c6d6a8317bc62af1cecf5346e218
StarrK <tony.kino.3@gmail.com>
2018-12-07 20:18:45 +0100

    Add indew view test

--- spec/views/static_pages/index.html.erb_spec.rb
+++ spec/views/static_pages/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'display the given text' do
+    render :plain => "Patisserie KN"
+    expect(rendered).to match /Patisserie KN/
+  end
 end
