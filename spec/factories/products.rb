a87fadb26877b188e36a1b56aa44eaef38bcab2a
StarrK <tony.kino.3@gmail.com>
2018-12-07 19:25:47 +0100

    Refactor product tests

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,16 +1,9 @@
 FactoryBot.define do
   factory :product do
-    name { "Cake" }
-    description { "A beautiful cake" }
-    price { "9.99" }
-    category { "Pastry" }
     cart
-
-    factory :ramdom_product do
-      name { Faker::Food.fruits }
-      description { Faker::Food.description }
-      price { Faker::Number.decimal(2, 2) }
-      category { Faker::Food.dish }
-    end
+    name { Faker::Food.fruits }
+    description { Faker::Food.description }
+    price { Faker::Number.decimal(2, 2) }
+    category { Faker::Food.dish }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,32 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  it { is_expected.to belong_to(:cart)}
+  it { is_expected.to validate_presence_of(:name)}
+  it { is_expected.to validate_presence_of(:description)}
+  it { is_expected.to validate_presence_of(:price)}
+  it { is_expected.to validate_presence_of(:category)}
 
-  let(:product) { build(:ramdom_product) }
+  it { is_expected.to validate_length_of(:name).
+                      is_at_least(3).is_at_most(63)}
 
-  it 'should be valid' do
-    expect(product.valid?).to be true
+  it { is_expected.to validate_length_of(:description).
+                      is_at_least(3).is_at_most(1023)}
+
+  it { is_expected.to validate_length_of(:category).
+                      is_at_least(3).is_at_most(63)}
+
+  let(:product) { create(:product) }
+  it 'is creatable' do
+    expect{ product }.to change(Product, :count).by(1)
   end
 
-  # Shoulda matchers tests
-  context 'validation tests' do
-    it { is_expected.to validate_presence_of(:name)}
-    it { is_expected.to validate_presence_of(:description)}
-    it { is_expected.to validate_presence_of(:price)}
-    it { is_expected.to validate_presence_of(:category)}
-
-    it { is_expected.to validate_length_of(:name).
-                        is_at_least(3).is_at_most(63)}
-
-    it { is_expected.to validate_length_of(:description).
-                        is_at_least(3).is_at_most(1023)}
-
-    it { is_expected.to validate_length_of(:category).
-                        is_at_least(3).is_at_most(63)}
-  end
-
-
-  context "Associations" do
-    it { is_expected.to belong_to(:cart)}
-  end
 end
