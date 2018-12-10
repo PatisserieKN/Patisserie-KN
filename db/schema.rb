c11ac209e38ccb17085e8fb44ff15a12d8209345
Tony StarrK <tony.kino.3@gmail.com>
2018-12-10 12:14:37 +0100

    Create migrate to add user to cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_207_134_811) do
+ActiveRecord::Schema.define(version: 2018_12_10_105833) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +19,8 @@ ActiveRecord::Schema.define(version: 20_181_207_134_811) do
     t.boolean "paid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -58,6 +61,7 @@ ActiveRecord::Schema.define(version: 20_181_207_134_811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "carts"
   add_foreign_key "profiles", "users"
 end
