3012a0a73c735241ad74452c11f89c2e2264854d
Félix Perez <felix2@hotmail.fr>
2018-12-12 19:05:08 +0100

    Resolving issues with Sendgrid and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'patisserie-felix.herokuapp.com',
+    :domain => 'patisserie-kn-staging.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
