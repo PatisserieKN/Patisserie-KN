ccc2189a0a76e8b2aba6c5487a55e8aa60db31b6
Tony StarrK <tony.kino.3@gmail.com>
2018-12-12 19:30:42 +0100

    Fix host name sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'patisserie-felix.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'patisserie-kn-staging.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
