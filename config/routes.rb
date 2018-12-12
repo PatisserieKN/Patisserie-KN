21904b9d6697c27c0c64dc8f0a5d00d6db8a59cb
Tony StarrK <tony.kino.3@gmail.com>
2018-12-12 14:13:12 +0100

    Change method to custom routes who works with custom controllers

--- config/routes.rb
+++ config/routes.rb
@@ -13,19 +13,22 @@ Rails.application.routes.draw do
   get '/template1', to: 'static_pages#template1'
   get '/template2', to: 'static_pages#template2'
   devise_for :users, controllers: { registrations: 'users/registrations'},
-                     skip: [:sessions, :registrations]
-
-  devise_scope :user do
-    get 'connexion', to: 'devise/sessions#new', as: :new_user_session
-    post 'connexion', to: 'devise/sessions#create', as: :user_session
-    delete 'deconnexion', to: 'devise/sessions#destroy', as: :destroy_user_session
-
-    get 'inscription', to: 'devise/registrations#new', as: :new_user_registration
-    post 'inscription', to: 'devise/registrations#create'
-    get 'profil', to: 'devise/registrations#edit', as: :edit_user_registration
-    patch 'profil', to: 'devise/registrations#update', as: :user_registration
-    put 'profil', to: 'devise/registrations#update'
-    delete 'users', to: 'devise/registrations#destroy' # default route
-    get '/users/cancel', to: 'devise/registrations#cancel' # default route
-  end
+                     path_names: { sign_in: 'connexion',
+                                   sign_out: 'deconnexion',
+                                   sign_up: 'inscription' }
+                                   
+  # Il semblerait que la méthode ci dessous ne fonctionne pas avec des controllers customs ?
+  # devise_scope :user do
+  #   get 'connexion', to: 'devise/sessions#new', as: :new_user_session
+  #   post 'connexion', to: 'devise/sessions#create', as: :user_session
+  #   delete 'deconnexion', to: 'devise/sessions#destroy', as: :destroy_user_session
+  #
+  #   get 'inscription', to: 'devise/registrations#new', as: :new_user_registration
+  #   post 'inscription', to: 'devise/registrations#create'
+  #   get 'profil', to: 'devise/registrations#edit', as: :edit_user_registration
+  #   patch 'profil', to: 'devise/registrations#update', as: :user_registration
+  #   put 'profil', to: 'devise/registrations#update'
+  #   delete 'users', to: 'devise/registrations#destroy' # default route
+  #   get '/users/cancel', to: 'devise/registrations#cancel' # default route
+  # end
 end
