33aa00c71a519ec995f25f35ca98393325eedd3a
Félix Perez <felix2@hotmail.fr>
2018-12-13 01:53:58 +0100

    CRUD for product created, adding the templates to the views, check PT for remainings errors

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,71 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def create
+   if current_user.admin == true
+    @product = Product.create(product_params)
+    if @product.save
+      flash[:success] = "Le produit a bien été créé"
+      redirect_to root_url
+    else
+      flash[:danger] = "Une erreur est survenue"
+    end
+   else
+    flash[:danger] = "Vous n'avez pas accès à cette section."
+    redirect_to root_url
+   end
+  end
+
+  def new
+   if current_user.admin == true
+    @product = Product.new
+   else
+    flash[:danger] = "Vous n'avez pas accès à cette section."
+    redirect_to root_url   
+   end
+  end
+
+  def edit
+   if current_user.admin == true
+    @product = Product.find(params[:id])
+   else
+    flash[:danger] = "Vous n'avez pas accès à cette section."
+    redirect_to root_url   
+   end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def update
+   if current_user.admin == true
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    flash[:success] = "Le produit a bien été modifié"
+    redirect_to root_url
+   else
+    flash[:danger] = "Vous n'avez pas accès à cette section."
+    redirect_to root_url   
+   end
+  end
+
+  def destroy
+   if current_user.admin == true
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:success] = "Le produit a bien été supprimé"
+    redirect_to root_url
+   else
+    flash[:danger] = "Vous n'avez pas accès à cette section."
+    redirect_to root_url   
+   end
+  end
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category, :cart_id)
+  end
+end
