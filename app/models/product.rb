7954f9da836e91192b5a4a430e3efe06d8519db4
Félix Perez <felix2@hotmail.fr>
2018-12-13 20:30:45 +0100

    Active storage image can be added by admin

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,7 @@
 
 class Product < ApplicationRecord
   belongs_to :cart
+  has_one_attached :image
   validates_presence_of :name, :description, :price, :category
   validates_length_of :name, in: 3..63, too_short: 'minimum 3 caractères', too_long: 'maximum 63 caractères'
   validates_length_of :description, in: 3..1023, too_short: 'minimum 3 caractères', too_long: 'maximum 1023 caractères'
