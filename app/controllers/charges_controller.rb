40af79c884fad1b22b370c1755b4eafc83c49537
Tony StarrK <tony.kino.3@gmail.com>
2018-12-16 23:17:46 +0100

    Transfer items from user to order when checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,21 +1,21 @@
 class ChargesController < ApplicationController
   def new
     if user_signed_in?
-      @order = Order.create(user_id: current_user.id)
-
-      # Tranfer items from user to order
+      global_price = 0
       current_user.items.each do |item|
-        item.buyable = @order
-        item.save
+        global_price += item.price
       end
-      @order.save
-
+      @order = Order.create(user_id: current_user.id, global_price: global_price)
     end
   end
 
   def create
-    # Amount in cents
-    @amount = 100
+    @order = Order.last
+
+    # Transfer item from user to order
+    current_user.items.each do |item|
+      item.update(buyable: @order)
+    end
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -24,13 +24,15 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+      :amount      => @order.global_price.to_i*100,
+      :description => 'Patisserie KN',
+      :currency    => 'eur'
     )
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
+
+    flash[:succes] = "Votre commande a bien été prise en compte. Vous pouvez la retrouver dans votre profil"
     redirect_to new_charge_path
   end
 end
