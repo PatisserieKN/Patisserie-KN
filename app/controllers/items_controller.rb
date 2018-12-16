5a5a11a019c830bc940a501bc2300b62bee571ab
Tony StarrK <tony.kino.3@gmail.com>
2018-12-16 13:02:38 +0100

    CRUD item terminé

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && @item.user == current_user
+      flash[:danger] = 'Vous ne pouvez pas faire ça !'
+      redirect_to root_path
+    end
   end
 
   def create
@@ -41,6 +46,30 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if user_signed_in? && @item.user == current_user
+      @item.update(item_params)
+      @item.price = @item.quantity * @item.product.price
+      @item.comment = nil if @item.comment.blank?
+      @item.personalization.blank? ? @item.personalization = nil : @item.price += 2.5
+      if @item.save
+        flash[:info] = 'Votre article a été mis à jour'
+        redirect_to items_path
+      else
+        error_message = "Votre article n'a pas pu être mis à jour pour la/les raison(s) suivante(s) : \n"
+        @item.errors.messages.each do |type|
+          error_message += "- #{type[0]} :\n"
+          type[1].each do |message|
+            error_message += "  - #{message}\n"
+          end
+        end
+        flash[:danger] = error_message
+        redirect_back(fallback_location: items_path)
+      end
+    else
+      flash[:danger] = 'Vous ne pouvez pas faire ça !'
+      redirect_to root_path
+    end
   end
 
   def destroy
@@ -58,6 +87,6 @@ class ItemsController < ApplicationController
   private
 
     def item_params
-      params.require(:item).permit(:title, :personalization, :quantity, :comment)
+      params.require(:item).permit(:personalization, :quantity, :comment)
     end
 end
