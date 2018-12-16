aaadd0bfaad4e98716aba70eb9cf8a7ef898cf17
Félix Perez <felix2@hotmail.fr>
2018-12-16 18:34:44 +0100

    Change column phone number to string in profiles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_144943) do
+ActiveRecord::Schema.define(version: 2018_12_16_173121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 2018_12_16_144943) do
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
